5660df93bb3b2a941ec537ca82b765393079ec9d
AliAnilKocak <alianilkocak@gmail.com>
2019-05-07 02:09:51 +0300

    home page created

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{config('app.locale')}}">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title',config('app.name'))</title>
+    @include('layouts.partials.head')
+    @yield('head')
 </head>
-<body>
+
+<body id="commerce">
 
 @include('layouts.partials.navbar')
-    <h1>@yield('content')</h1>
-@include('layouts.partials.footer',['year'=>2019])
+@yield('content')
+@include('layouts.partials.footer')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
+    @yield('footer')
 </body>
 </html>

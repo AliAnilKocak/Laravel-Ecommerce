b1427e49e9b9bcf02fba5794ab83bf86293f5bd8
AliAnilKocak <alianilkocak@gmail.com>
2019-05-08 02:34:03 +0300

    Product page created

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Redirect;
 Route::get('/', 'HomePageController@index');
 
 Route::get('/category/{slug_categoryname}', 'CategoryController@index')->name('category');
-Route::view('/product', 'product');
+
+Route::get('/product/{slug_productname}', 'ProductController@index')->name('product');
+
+
+
 Route::view('/sepet', 'sepet');
 
 

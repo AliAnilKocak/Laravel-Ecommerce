3e6ab0701c3e4edd9e26e71a372c0143b3db7756
AliAnilKocak <alianilkocak@gmail.com>
2019-05-11 15:26:26 +0300

    Product Model, Migration, Seeder and Product_Category migration created

--- database/migrations/2019_05_11_121843_create_category_product_table.php
+++ database/migrations/2019_05_11_121843_create_category_product_table.php
@@ -8,14 +8,23 @@ class CreateCategoryProductTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     *v
      * @return void
      */
     public function up()
     {
         Schema::create('category_product', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->integer('category_id')->unsigned(); //sadece pozitif sayılar
+            $table->integer('product_id')->unsigned();
+
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade');
+            //category tablosundaki id'yi category_id'ye iliştirmiş oldum
+            //Çoka çok M:M ilişki oluşturdum.
+            //cascade bir ürün veya kategori silindiğinde bu tablodan da sil anlamında.
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
+            //product tablosundaki id'yi product_id'ye iliştirmiş oldum
+
         });
     }
 

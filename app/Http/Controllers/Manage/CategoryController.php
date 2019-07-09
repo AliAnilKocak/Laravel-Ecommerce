eb42bd071109a06cabed169edd5eb0e02a31cd7b
AliAnilKocak <alianilkocak@gmail.com>
2019-07-09 16:48:27 +0300

    category delete

--- app/Http/Controllers/Manage/CategoryController.php
+++ app/Http/Controllers/Manage/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
     public function index()
     {
         if (request()->filled('search_word')) {
-            request()->flash();// eski form verisi sayfa yenilendiğinde kalması için requestteki bilgileri sessiona flashladık.
+            request()->flash(); // eski form verisi sayfa yenilendiğinde kalması için requestteki bilgileri sessiona flashladık.
             $search_word = request('search_word');
             $list = Category::where('name', 'like', "%$search_word%")
                 ->orWhere('slug', 'like', "%$search_word%")->orderByDesc('created_at')->paginate(8);
@@ -27,50 +27,49 @@ class CategoryController extends Controller
         if ($id > 0) {
             $entry = Category::find($id);
         }
-        return view('manage.category.form', compact('entry'));
+        $categories = Category::all();
+        return view('manage.category.form', compact('entry', 'categories'));
     }
 
     public function save($id = 0)
     {
+        $data = request()->only('name', 'slug', 'parent_id');
+        if (!request()->filled('slug')) {
+            $data['slug'] = str_slug(request('name'));
+            request()->merge(['slug' => $data['slug']]);
+        }
+
+
         $this->validate(request(), [
-            'email' => 'required',
-            'full_name' => 'required'
+            'name' => 'required',
+            'slug' => request('original_slug') != request('slug') ?  'unique:category,slug' : '',
         ]);
 
-        $data = request()->only('email', 'full_name');
-        if (request()->filled('password')) {
-            $data['password'] = Hash::make(request('password'));
-        }
 
-        $data['is_active'] = request()->has('is_active') ? 1 : 0;
-        $data['is_admin'] = request()->has('is_admin') ? 1 : 0;
+        /*if(Category::whereSlug($data['slug'])->count() > 0)
+        return back()->withInput()->withErrors(['slug'=>'Slug değer daha önceden kayıtlıdır']);*/
 
         if ($id > 0) { //update
-            $entry = _UsersModel::where('id', $id)->firstOrFail();
+            $entry = Category::where('id', $id)->firstOrFail();
             $entry->update($data);
         } else { //create
-            $entry = _UsersModel::create($data);
+            $entry = Category::create($data);
         }
-
-        UserDetail::updateOrCreate(
-            ['user_id' => $entry->id],
-            [
-                'adress' => request('adress'),
-                'number' => request('number'),
-                'tel_number' => request('tel_number')
-            ]
-        );
-
-        return  redirect()->route('manage.user.edit', $entry->id)
+        return  redirect()->route('manage.category.edit', $entry->id)
             ->with('message', ($id > 0 ? "Güncellendi" : "Yeni bir kayıt oluşturuldu"))
             ->with('message_type', 'success');
     }
 
     public function delete($id)
     {
-        _UsersModel::destroy($id);
-        return redirect()->route('manage.user')
+
+
+        $category = Category::find($id);
+        $category->products()->detach(); //kategoriye ait ürünleri siler
+        $category->delete();
+
+        return redirect()->route('manage.category')
             ->with('message_type', 'success')
-            ->with('message', 'Kullanıcı silinmiştir.');
+            ->with('message', 'Kategori silinmiştir.');
     }
 }
--- resources/views/manage/category/form.blade.php
+++ resources/views/manage/category/form.blade.php
@@ -1,84 +1,41 @@
 @extends('manage.layouts.master')
 @section('title','Ana Sayfa')
 @section('content')
-<h1 class="page-header">Kullanıcı Yönetimi</h1>
-
-<form method="post" action="{{route('manage.user.save',@$entry->id)}}">
+<h1 class="page-header">Kategori Yönetimi</h1>
+<form method="post" action="{{route('manage.category.save',@$entry->id)}}">
     <div class="pull-right">
         <button type="submit" class="btn btn-primary">
             {{@$entry->id>0 ? "Güncelle" : "Kaydet" }}
         </button>
     </div>
-    <h1 class="sub-header">{{@$entry->id>0 ? "Güncelleme" : "Yeni Kayıt" }} formu </h1>
-    {{ csrf_field() }}
-    @include('layouts.partials.alert')
-    @include('layouts.partials.errors')
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="full_name">Ad Soyad</label>
-                <input type="text" name="full_name" class="form-control" value="{{old('full_name',$entry->full_name)}}" id="full_name"
-                    placeholder="Ad Soyad">
+    <h3 class="sub-header">{{@$entry->id>0 ? "Güncelleme" : "Yeni Kayıt" }} formu </h1>
+        {{ csrf_field() }}
+        @include('layouts.partials.alert')
+        @include('layouts.partials.errors')
+        <div class="row">
+            <div class="form-group row">
+                <div class="col-sm-4">
+                    <select class="form-control" name="parent_id" id="parent_id">
+                        <option value="">Ana Kategori</option>
+                        @foreach ($categories as $category)
+                        <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-4">
+                    <input type="name" name="name" class="form-control" id="name" value="{{old('full_name',$entry->name)}}"
+                        placeholder="Kategori ismi">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-4">
+                    <input type="hidden" name="original_slug" id="original_slug" value="{{old('slug',$entry->slug)}}">
+                    <input type="text" name="slug" class="form-control" id="slug" value="{{old('slug',$entry->slug)}}"
+                        placeholder="Slug">
+                </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" name="email" class="form-control" value="{{old('email',$entry->email)}}" id="email"
-                    placeholder="Email">
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name="password"  class="form-control" id="password" placeholder="Password">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="adress">Address</label>
-                <input type="text" class="form-control" id="adress" name="adress" value="{{old('adress',$entry->detail->adress)}}"
-                    placeholder="Address">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="telephone">Telefon</label>
-                <input type="text" class="form-control" id="telephone" name="tel_number"
-                    value="{{old('tel_number',$entry->detail->tel_number)}}" placeholder="Telephone">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="mobilephone">Cep telefonu</label>
-                <input type="text" class="form-control" id="mobilephone" value="{{old('number',$entry->detail->number)}}"
-                    name="number" placeholder="Cep Telephone">
-            </div>
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="exampleInputFile">File input</label>
-        <input type="file" id="exampleInputFile">
-        <p class="help-block">Example block-level help text here.</p>
-    </div>
-    <div class="checkbox">
-        <label>
-            <input type="hidden" name="is_active" value="0" id="">
-            <input name="is_active" value="1" {{old('is_active',$entry->is_active) ? 'checked' : '' }} type="checkbox"> Aktif mi
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-                <input type="hidden" name="is_admin" value="0" id="">
-
-            <input name="is_admin" value="1" {{old('is_admin',$entry->is_admin) ? 'checked' : '' }} type="checkbox"> Admin mi
-        </label>
-    </div>
 </form>
 @endsection
